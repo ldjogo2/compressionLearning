874aa442e29d64b04d13f48fe69c9bd3c726add1
ldjogo2 <bball.luka.hotmail.com@gmail.com>
2021-12-30 17:09:05 -0600

    Final tuneup

--- huffmanComp/huffmanAlgo.cpp
+++ huffmanComp/huffmanAlgo.cpp
@@ -1,3 +1,5 @@
+// Credit: Geeks for Geeks Huffman Coding
+
 #include <string>
 #include <vector>
 #include <unordered_map>
@@ -6,48 +8,35 @@
 
 using namespace std;
 
-// A Huffman tree node
+// Huffman tree node
 struct MinHeapNode {
-  
-    // One of the input characters
     char data;
-  
-    // Frequency of the character
     unsigned freq;
   
-    // Left and right child
     MinHeapNode *left, *right;
   
     MinHeapNode(char data, unsigned freq)
-  
     {
-  
         left = right = NULL;
         this->data = data;
         this->freq = freq;
     }
 };
   
-// For comparison of
-// two heap nodes (needed in min heap)
+// MinHeap needed
 struct compare {
-  
     bool operator()(MinHeapNode* l, MinHeapNode* r)
-  
     {
         return (l->freq > r->freq);
     }
 };
   
-// Prints huffman codes from
-// the root of Huffman Tree.
 void printCodes(struct MinHeapNode* root, string str)
 {
-  
     if (!root)
         return;
   
-    if (root->data != '$')
+    if (root->data != '$')  // must be leaf node 
         std::cout << root->data << ": " << str << "\n";
   
     printCodes(root->left, str + "0");
@@ -55,46 +44,30 @@ void printCodes(struct MinHeapNode* root, string str)
 }
   
 
-// The main function that builds a Huffman Tree and
-// print codes by traversing the built Huffman Tree
+// Main function which builds Huffman Tree and prints codes generated from tree
 void HuffmanCodes(std::vector<char> data, std::vector<int> freq, int size)
 {
     struct MinHeapNode *left, *right, *top;
-  
-    // Create a min heap & inserts all characters of data[]
     std::priority_queue<MinHeapNode*, vector<MinHeapNode*>, compare> minHeap;
-  
     for (int i = 0; i < size; ++i)
         minHeap.push(new MinHeapNode(data[i], freq[i]));
   
-    // Iterate while size of heap doesn't become 1
     while (minHeap.size() != 1) {
-  
-        // Extract the two minimum
-        // freq items from min heap
+        // getting next two minimum values to insert into tree
         left = minHeap.top();
         minHeap.pop();
-  
         right = minHeap.top();
         minHeap.pop();
   
-        // Create a new internal node with
-        // frequency equal to the sum of the
-        // two nodes frequencies. Make the
-        // two extracted node as left and right children
-        // of this new node. Add this node
-        // to the min heap '$' is a special value
-        // for internal nodes, not used
+        // inserting two min values into tree
         top = new MinHeapNode('$', left->freq + right->freq);
-  
         top->left = left;
         top->right = right;
   
         minHeap.push(top);
     }
-  
-    // Print Huffman codes using
-    // the Huffman tree built above
+
+    // print the codes from the tree
     printCodes(minHeap.top(), "");
 }
   
@@ -117,6 +90,11 @@ int main (int argc, char *argv[]) {
         }
         else {
             ch[c] = 1;
+        }
+
+        if (c == '$') {
+            std::cout << "Cannot include $ as it is used to indicate non-leaf nodes in HuffmanTree!" << std::endl;
+            return 0;
         }
     }
 
