078bd0ce82d8325fc9226121f596355198c89b61
ldjogo2 <bball.luka.hotmail.com@gmail.com>
2021-12-30 14:35:43 -0600

    First commit

---
+++ huffmanComp/huffmanAlgo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Problem with input for compression!" << std::endl;
+        return 0;
+    }
+
+    std::string input = argv[1];
+    
+
+    return 0;
+}
